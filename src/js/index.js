4eb72b00cae29cd4307775786e51e8af23a25de4
yun-sangho <contact@yunsangho.com>
2020-04-12 00:46:40 +0900

    implemented product section

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,44 @@
-import * as product from '../asset/product.json';
+import * as product from '../../asset/product.json';
 import { productFactory } from './product';
 
 const app = document.getElementById('app');
 
 product.data.forEach((el) => {
   const productDOM = productFactory(el);
-  app.appendChild(productDOM);
+  const section = document.createElement('div');
+
+  section.className = 'section';
+  section.appendChild(productDOM);
+  app.appendChild(section);
 });
+
+// const sections = [...document.querySelectorAll('.section')];
+
+// let options = {
+//   rootMargin: '0px',
+//   threshold: 0.75,
+// };
+
+// const callback = (entries) => {
+//   entries.forEach((entry) => {
+//     const { target } = entry;
+
+//     if (entry.intersectionRatio >= 0.75) {
+//       target.classList.add('is-visible');
+//     } else {
+//       target.classList.remove('is-visible');
+//     }
+//   });
+// };
+
+// const observer = new IntersectionObserver(callback, options);
+
+// sections.forEach((section) => {
+//   const sectionChildren = [...section.querySelector('[data-content]').children];
+
+//   sectionChildren.forEach((el, index) => {
+//     el.style.setProperty('--delay', `${index * 150}ms`);
+//   });
+
+//   observer.observe(section);
+// });
--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,18 @@
 import { createHtmlString, createDOMFromString } from './util';
+import images from '../../asset/img/*.png';
 
 const productHTML = `
-  <div>
-    <img src="{src}">{name}</img>
-    <div>{description}</div>
+  <div class="product" data-content>
+    <img class="product__img" src="{src}"></img>
+    <div class="product__description">
+      <h2>{description}</h2>
+    </div>
   </div>
 `;
 
 const createProductString = createHtmlString(productHTML);
 
 export const productFactory = (variables) => {
+  variables.src = images[variables.name];
   return createDOMFromString(createProductString(variables));
 };
